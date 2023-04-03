a40e22ebbca7d97be1d6721ebb6280208435bb4c
Rupendra <Rupendra@LAPTOP-IUJNIELA>
2023-04-03 16:26:08 +0530

    2nd commit

---
+++ src/test/java/Sample/Ex.java
@@ -0,0 +1,10 @@
+package Sample;
+
+public class Ex {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/Sample/Example.java
@@ -0,0 +1,12 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Example {
+
+	
+	@Test
+	public void test() {
+		System.out.println("helooo");
+	}
+}
