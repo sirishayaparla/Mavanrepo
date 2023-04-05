8a777ac732ea27dbecc56bc1c0b5cbbca76aca5a
Rupendra <Rupendra@LAPTOP-IUJNIELA>
2023-04-05 13:20:42 +0530

    upda

--- src/test/java/Sample/TS_04.java
+++ src/test/java/Sample/TS_04.java
@@ -1,10 +1,16 @@
 package Sample;
 
+import org.testng.annotations.Test;
+
 public class TS_04 {
 
-	public static void main(String[] args) {
+	@Test
+	public static void test(){
 
 		System.out.println("jenkins");
+		
+		
+		
 		
 	}
 
